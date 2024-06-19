886120127a5ad44864b97fccde4ced7e16960883
Justice London <jlondon@syrussystems.com>
2024-06-19 16:51:30 -0700

    Allow management of subnets

--- r-network.tf
+++ r-network.tf
@@ -1,3 +1,12 @@
+
+resource "azurerm_role_assignment" "aks_uai_subnets_network_contributor" {
+  for_each = toset(var.nodes_subnet_id)
+
+  scope                = each.key
+  principal_id         = azurerm_user_assigned_identity.aks_user_assigned_identity.principal_id
+  role_definition_name = "Network Contributor"
+}
+
 resource "azurerm_role_assignment" "aks_uai_route_table_contributor" {
   count = var.aks_network_plugin == "kubenet" && var.outbound_type == "userDefinedRouting" ? 1 : 0
 
